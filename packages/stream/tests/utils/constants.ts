ed64c85228821fdfc2ffca1589541c090540f36a
Huzaifa181 <huzaifa.ahmed181@gmail.com>
2022-07-04 13:59:09 +0500

    Add: test cases of native token of deposit, withdraw, start, cancel, withdraw streamed
    Other than these function have some issue, can not test completely which includes pause and resume stream for native token and functions of spl token

--- packages/stream/tests/utils/constants.ts
+++ packages/stream/tests/utils/constants.ts
@@ -3,10 +3,10 @@ export const SPL_TOKEN_ADDRESS = "83JcaM6oVGQamm6D27wSZmc7sEajKmJoxKs3WG4FSPqb";
 export const RECEIVER_ADDRESS = "8zERi9tcZPfNPfqLMWvnyFEabArGLzkw491smyg3RvVk";
 //private key for sender
 export const SENDER_SECRET_KEY =
-  "27hTGStLQRzDXYJ52o4gXHczGNL8p57ssezuey3TJxcrU2LYCa5ZJ9doitdCw6dGSc7CTDQU7uT9T1Y9R8j6Rqto";
+  "sender_private_key";
 // private key for receiver
 export const RECEIVER_SECRET_KEY =
-  "2UJgVNvNDG3uD5WnXJDBHg3ctvq29GL5VVjK38DyoVUdE8uPZiT4qXKptrXkPwdkD2ibwz7sJRnh3Po4ALX2chqp";
+  "receiver_private_key";
 export const PROGRAM_ID = "AknC341xog56SrnoK6j3mUvaD1Y7tYayx1sxUGpeYWdX";
 export const ZEBEC_FEE_IN_PERCENT = 0.25;
 export const NATIVE_AMOUNT = 10;
